ade3d329b06dc34d53d2d1b4fdbbcd34a47cf97c
smdooley <sean.dooley.22@gmail.com>
2024-12-17 15:19:59 +0000

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Sean Dooley on 17/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
